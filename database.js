14b9385fa704009b410e4973b11b87815ba511a2
Antoine Nauleau <ajnauleau@gmail.com>
2018-09-11 17:04:33 -0700

    Routes Cleared

--- database.js
+++ database.js
@@ -1,9 +1,37 @@
-/* Mongo/Mongoose Database Connection */
+/* Mongo Database Connection */
+
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+// Database Name
+const dbName = "theatres";
+
+// Use connect method to connect to the server
+MongoClient.connect(
+  url,
+  function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    client.close();
+  }
+);
+
+module.exports = MongoClient.connection;
+
+/* Mongoose */
+
+/*
 
 const mongoose = require("mongoose");
 assert = require("assert");
 
-const url = "mongodb://localhost/reddit-clone";
+const url = "mongodb://localhost/theatres/";
 mongoose.Promise = global.Promise;
 mongoose.connect(
   url,
@@ -22,3 +50,5 @@ mongoose.connection.on(
 mongoose.set("debug", true);
 
 module.exports = mongoose.connection;
+
+*/
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // call requires
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoDB = require('mongodb');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const mongoDB = require("mongodb");
 
 // express modules
 const router = express.Router();
@@ -13,7 +14,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator()); // Add after Body parser!
 
 // connect to db
-require('./database.js');
+const db = require("./database.js");
+
+// serve it up
+app.set("port", process.env.PORT);
+app.set("port", 3000);
+
+if (!module.parent) {
+  app.listen(app.get("port"), () => {
+    console.log("Server is running on port 3000");
+  });
+}
+
+/*
 
 theatres;
 sessions;
@@ -29,26 +42,71 @@ carts;
   Theatres > secondaries
   Sessions > primaries
 
+  Route path: /users/:userId/books/:bookId
+  Request URL: http://localhost:3000/users/34/books/8989
+  req.params: { "userId": "34", "bookId": "8989" }
+
+*/
+
 // routes
+app.use("/", router);
 
-router.get('/theater/name-of-route', function(req, res) {
-  res.json({'stub': `[${req.originalUrl}] Endpoint works! Replace me in Step 2.`});
+router.get("/theater/name-of-route", function(req, res) {
+  res.json({
+    stub: `[${req.originalUrl}] Endpoint works! Replace me in Step 2.`
+  });
+
+  let theaterId = 1;
+  let theaters = db.getSisterDB("booking").theaters;
+  let sessions = db.getSisterDB("booking").sessions;
+
+  let theater = theaters.findOne({ _id: theaterId });
+  sessions.insert({
+    name: "Action Movie 5",
+    description: "Another action movie",
+    start: ISODate("2015-03-11T15:00:00.000Z"),
+    end: ISODate("2015-03-11T16:00:00.000Z"),
+    price: 10,
+    seatsAvailable: theater.seatsAvailable,
+    seats: theater.seats,
+    reservations: []
+  });
 });
 
-app.get('/:sessions/', function(req, res) {
+app
+  .route("/sessions/:session/")
+  .get(function(req, res) {
+    res.send("Get a session: " + req.params.session);
+  })
+  .post(function(req, res) {
+    res.send("Post a session: " + req.params.session);
+  });
 
-});
+app
+  .route("/reserves/:reserve/")
+  .get(function(req, res) {
+    res.send("Get a session: " + req.params.reserve);
+  })
+  .post(function(req, res) {
+    res.send("Post a session: " + req.params.reserve);
+  });
 
-app.get('/:reserve/', function(req, res) {
+app
+  .route("/carts/:cart/")
+  .get(function(req, res) {
+    res.send("Get a session: " + req.params.cart);
+  })
+  .post(function(req, res) {
+    res.send("Post a session: " + req.params.cart);
+  });
 
-});
-
-app.get('/:carts/', function(req, res) {
-
-});
-
-app.get('/:receipt/', function(req, res) {
-
-});
+app
+  .route("/receipts/:receipt/")
+  .get(function(req, res) {
+    res.send("Get a session: " + req.params.receipt);
+  })
+  .post(function(req, res) {
+    res.send("Post a session: " + req.params.receipt);
+  });
 
 module.exports = router;
